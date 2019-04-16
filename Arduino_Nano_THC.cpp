2c3ee7497fa9d656b24c0e074d61b20dcde3106a
ETPC10 <40269341+ETPC10@users.noreply.github.com>
2019-04-16 09:59:37 +0200

    Update Arduino_Nano_THC.cpp
    Faster, now works with absolut hight

--- Arduino_Nano_THC.cpp
+++ Arduino_Nano_THC.cpp
@@ -3,136 +3,118 @@
 //Use of "Pierce Delay" and "Pause at the end of cut" may be a good Idea
 //Because it only works when ArcOK is true, it is save to use a Laser on the Plasma Table.
 
- 
-
-/*
- 
-How it should work:
-
-If the torch has fired up, the comparator board sets ArcOk signal to hight
-
-The micro controller sets the DIR and drives up the torch for a specific hight. Then checks the pin again. 
-
-If the comparator signals that the torch is high enough (voltage inside death zone), nothing will be done.
-
-If the voltage is too high, the comparator will send a Signal. Same process as up, but different direction of movement.
-
-After turning off the torch, torch is moved back to 0.
 
 
-*/
-
-
-float mm_already_moved = 0; //Stores hight of torch. 
-bool wasok=0; //true = torch was already moved, false = torch was not moved.
+float pos_old = 0; //Stores hight of torch. 
+bool wasok = 0; //true = torch was already moved, false = torch was not moved.
 
 void setup() {
-  pinMode(2, INPUT);   //Torch to low
-  pinMode(3, INPUT);   //Torch to hight
-  pinMode(4, INPUT);   //Arc OK
-  pinMode(11, OUTPUT); //Step Pin
-  pinMode(12, OUTPUT); //DIR Pin
+	pinMode(2, INPUT);   //Torch to low
+	pinMode(3, INPUT);   //Torch to hight
+	pinMode(4, INPUT);   //Arc OK
+	pinMode(11, OUTPUT); //Step Pin
+	pinMode(12, OUTPUT); //DIR Pin
 } //END of setup()
 
-void movetorch(bool dir){
-  //1=up 0=down
+void movetorch(bool dir, int steps) {
+	//1=up 0=down
+	int StepPin = 11;
+	int DirPin = 12;
+	int i;
 
-  float WayToMove = 0.100; //Way Axis moves per controller run[mm];    
-  float TravelMax_UP = 5; //Maximum way torch travels up from first Arc Ok [mm].
-  float TravelMax_DOWN = 0; //Maximum way torch travels down from first Arc Ok [mm]. Should be <0;
+	if (dir = 1) {                  //Change if direction is reversed (1 form 2 spots)
+		digitalWrite(DirPin, HIGH);	//UP		
+	}
+	else {
+		digitalWrite(DirPin, LOW);  //DOWN
+	}
 
 
-  int StepsToMove; //Number of Steps Axis moves
-  int StepPin = 11;
-  int DirPin = 12;
-  float StepsPermm = 200; //nachschauen //depends on your machine.
-  int msBetweenSteps = 1; // milliseconds, more = slower
-  int i;
-  bool MoveIsAllowed = false;
-
-   
-  if (dir=1){ //  UP                                                  Change if direction is reversed (1 form 2 spots)
-    if(mm_already_moved<=TravelMax_UP){ //Checks if Axis hasn't moved down to far.
-      digitalWrite(DirPin, HIGH); 
-      mm_already_moved+=WayToMove; //adds WayToMove to mm_already_moved; (Positive direction)
-      MoveIsAllowed = true; //Gives clearance for moving torch
-      } 
-  }    
-
-  
-  if (dir=0){ //Down                                                  Change if direction is reversed (2 form 2 spots)
-    if(mm_already_moved>=TravelMax_DOWN){ //Checks if Axis hasn't moved down to far.
-      digitalWrite(DirPin, LOW); 
-      mm_already_moved-=WayToMove; //adds WayToMove to mm_already_moved; (Negative direction)
-      MoveIsAllowed = true; //Gives clearance for moving torch
-      } 
-  }  
-
-  
-  StepsToMove =  WayToMove * StepsPermm ; //calculates Steps required to move WayToMove mm. Result is int because it must be in N|.
-  
-  if(MoveIsAllowed==true){//only moves torch if clearance is given 
-
-      for ( i = 0; i <= StepsToMove; i++){        //Moves Torch WayToMove mm / StepsToMove
-        digitalWrite(DirPin, HIGH);
-        digitalWrite(DirPin, LOW);
-        delay(msBetweenSteps); //slows down movement of torch
-      }
-
-  }
+	for (i = 0; i <= steps; i++) {        
+			digitalWrite(DirPin, HIGH);
+			digitalWrite(DirPin, LOW);
+			delay(1); //slows down movement of torch, may depend on stepper driver 
+	}
 }// END of movetorch() 
 
-void loop() {
-  int lowPin = 2;  
-  int highPin = 3;    
-  int okPin = 4;
-  bool lp, hp, okp;
-  float tolerance =0.05;  //tolerance for going back to 0mm hight; 
- 
-  //delay(20); //Slows down everything to avoid too fast actions, probably unnecessary.
-  
-  okp=digitalRead(okPin);//debounce
-  delay (5);
-  if(digitalRead((okPin)==okp)&&(okp==1)){ //True if not bouncing and pin on high
-   //If Arc is OK
+void mmts( int pos_new) { //function for math stuff. 
+	int stepsmm = 273; //Machine Specific 
+	int steps;
+	float way;
+	int softlimit = 5; //Maximum Way torch will travel up. Starts in the middel of it. i.e. if its set to 5 it will travel 2.5mm up 
+	bool dir ;
 
-      wasok=true; //Sets wasok to true, required for later reseting hight to 0;
-      
- 
-      //reads low-pin 
-      lp=digitalRead(lowPin);//debounce
-      delay (5);
-      
-      if(digitalRead((lowPin)==lp)&&(lp==1)){ //True if not bouncing and pin on high
-        movetorch(1);  //lifts torch
-      }
-      
-      else{ //Else avoids dual movement in one code run, maybe suppresses oscillations 
-      
-         //reads hight-pin  
-         hp=digitalRead(highPin);//debounce
-         delay (5);
-         
-         if(digitalRead((highPin)==hp)&&(hp==1)){ //True if not bouncing and pin on high
-          movetorch(0);  //lowers torch
-         }   
-      }
-  }
-  else{
-      //if arc is not ok, torch is off (should be at least)
-      if(wasok==true){
-        
-        if(mm_already_moved > tolerance){movetorch(0);} //if torch is above tolerance hight, torch will be lowered;
-        
-        if(mm_already_moved < ((-1)*tolerance)){movetorch(1);}//if torch is lower then lower tolerance limit, torch will be raised
-        
-        if((mm_already_moved < tolerance)&&(mm_already_moved > ((-1)*tolerance))){wasok=false;}  //if torch is inside the tolerance, 
-                                                                                                  //wasok will be set false. Cutting cycle completed        
-          
-      }   
- 
-  }
-  
+	way = pos_new - pos_old;  //Calcuates the way to travel 
+	
+	if (way > 0) {   //checks if move is up or down
+		dir = 1;
+	}
+
+	if (way < 0) {
+		dir = 0;
+		way = way * (-1);
+	}
+
+	if (pos_new < (softlimit*0.5)) {	//checks if move is legal
+		steps = way * stepsmm;
+		movetorch(dir, steps);
+		pos_old = pos_new;
+	}
+	else {
+	//May add warning LED
+	}
+
+} //End of mmts()
+
+
+void loop() {
+	int lowPin = 2;
+	int highPin = 3;
+	int okPin = 4;
+	int i;
+	bool lp, hp, okp;
+	float delta = 0.05; //Way torch travels per run. Must be bigger than 1/(Steps per mm of machine)
+
+	
+
+	//delay(20); //Slows down everything to avoid too fast actions, probably unnecessary.
+
+	okp = digitalRead(okPin);//debounce
+	delay(5);
+	if (digitalRead((okPin) == okp) && (okp == 1)) { //True if not bouncing and pin on high
+	 //If Arc is OK
+
+		wasok = true; //Sets wasok to true, required for later reseting hight to 0;
+
+
+		//reads low-pin 
+		lp = digitalRead(lowPin);//debounce
+		delay(5);
+
+		if (digitalRead((lowPin) == lp) && (lp == 1)) { //True if not bouncing and pin on high
+			mmts((pos_old+delta));  //lifts torch
+		}
+
+		else { //Else avoids dual movement in one code run, maybe suppresses oscillations 
+
+		   //reads hight-pin  
+			hp = digitalRead(highPin);//debounce
+			delay(5);
+
+			if (digitalRead((highPin) == hp) && (hp == 1)) { //True if not bouncing and pin on high
+				mmts((pos_old - delta));  //lowers torch
+			}
+		}
+	}
+	else {
+		//if arc is not ok, torch is off (should be at least)
+		if (wasok == true) {
+			mmts(0); //Moves torche back to 0;
+			wasok = false;
+		}
+
+	}
+
 }// END of loop()
 
 // There's always one more bug. 
